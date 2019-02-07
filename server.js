6259080eb7065e9570b96a3cf4080c9593fe6992
Alicia Hetrick <ahetrick22@gmail.com>
2019-02-07 16:07:32 -0500

    wrote tests for POST /word_count_per_sentence

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const CONTENT_HEADERS = {"Content-Type": "application/json"};
 const PORT = process.env.PORT || 5001;
 
 //allow express app to correctly parse parameters from the body
@@ -15,53 +14,62 @@ app.use(
 //receives a JSON payload in the request body with key 'paragraph' and a string value of 2+ sentences, 
 //returns JSON with key 'sentences' and an array value containing objects with text and wordCount properties
 app.post('/word_count_per_sentence', (req, res) => {
-  let { paragraph } = req.body;
-  //make sure the request has sent a paragraph
-  if (paragraph) {
-    //remove whitespace on either side 
-    paragraph = paragraph.trim();
-    //we only want to have to iterate over the entire string once, 
-    //so identify both the spaces and sentence endings in one place and 
-    //then save it in a return obj
-    const returnObj = {
-      sentences: []
-    };
-    //track the previous end so that we know where to split each sentence
-    let previousSentenceEnd = 0;
-    let singleSentence = {
-      text: '', 
-      wordCount: 1
-    };
-    for (let c = 0; c < paragraph.length; c++) {
-      //if the character is a space, then track a new word
-      if (paragraph[c] === ' ') {
-        singleSentence.wordCount++;
-      }
-      //if the character is a sentence ender, then slice off the sentence and push a copy of 
-      //it into the return, then reset the object to track the next sentence
-      if(paragraph[c] === '.' || paragraph[c] === '?' || paragraph[c] === '!') {
-        singleSentence.text = paragraph.slice(previousSentenceEnd, c+1).trim();
-        returnObj.sentences.push({...singleSentence});
-        previousSentenceEnd = c;
-        singleSentence.text = '';
-        singleSentence.wordCount = 1;
-        //bump up to the next index that has a letter after the end of the sentence 
-        //this allows for any number of spaces after a sentence's ending punctuation
-          while (paragraph[c+1] === ' ') {
-            c++
-          };
+  //make sure there's a body sent
+  if (req.body) {
+    let { paragraph } = req.body;
+    //make sure the request has sent a paragraph
+    if (paragraph) {
+      //remove whitespace on either side 
+      paragraph = paragraph.trim();
+      //we only want to have to iterate over the entire string once, 
+      //so identify both the spaces and sentence endings in one place and 
+      //then save it in a return obj
+      const returnObj = {
+        sentences: []
       };
-    };
-    //make sure there is more than one sentence in the paragraph, since that was a requirement
-    if (returnObj.sentences.length < 2) {
-      res.status(400).send('Bad request - your paragraph must have 2 or more sentences to be processed');
-    };
-    res.send(JSON.stringify(returnObj));
+      //track the previous end so that we know where to split each sentence
+      let previousSentenceEnd = 0;
+      let singleSentence = {
+        text: '', 
+        wordCount: 1
+      };
+      for (let c = 0; c < paragraph.length; c++) {
+        //if the character is a space, then track a new word
+        if (paragraph[c] === ' ') {
+          singleSentence.wordCount++;
+        }
+        //if the character is a sentence ender, then slice off the sentence and push a copy of 
+        //it into the return, then reset the object to track the next sentence
+        if(paragraph[c] === '.' || paragraph[c] === '?' || paragraph[c] === '!') {
+          singleSentence.text = paragraph.slice(previousSentenceEnd, c+1).trim();
+          returnObj.sentences.push({...singleSentence});
+          previousSentenceEnd = c;
+          singleSentence.text = '';
+          singleSentence.wordCount = 1;
+          //bump up to the next index that has a letter after the end of the sentence 
+          //this allows for any number of spaces after a sentence's ending punctuation
+          //also accounts for two punctuation marks in a row (like !?)
+            while (paragraph[c+1] === ' ' || paragraph[c+1] === '.' || paragraph[c+1] === '!'|| paragraph[c+1] === '?') {
+              c++
+            };
+        };
+      };
+      //make sure there is more than one sentence in the paragraph, since that was a requirement
+      if (returnObj.sentences.length < 2) {
+        res.status(400).send('Bad request - your paragraph must have 2 or more sentences to be processed');
+      } else {
+        //send the result
+        res.setHeader("Content-type", "application/json");
+        res.send(JSON.stringify(returnObj));
+      }
+    } else {
+      //handle if no paragraph was sent
+      res.status(400).send('Bad request - must send a paragraph in the body');
+    }
   } else {
-    //handle if no paragraph was sent
-    res.status(400).send('Bad request - must send a paragraph in the body');
+    //handle if no body was sent
+    res.status(400).send('Body is required');
   }
-
 })
 
 //For each case-insensitive letter, return the number of times that letter appears in the entire text. 
@@ -74,3 +82,6 @@ app.post('/total_letter_count', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
+
+//export for testing
+module.exports = app;
--- test/test.js
+++ test/test.js
@@ -2,7 +2,84 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../server');
 const should = chai.should();
-const expect = chai.expect;
-const assert = chai.assert;
-
 chai.use(chaiHttp);
+
+describe('POST /word_count_per_sentence', () => {
+  it('should require a body in the request', done => {
+    chai.request(server)
+      .post('/word_count_per_sentence')
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.empty;
+      })
+      done();
+  })
+
+  it('should have a key paragraph in the body of the request', done => {
+    chai.request(server)
+      .post('/word_count_per_sentence')
+      .send({NotAParagraph: "blah blah blah"})
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.empty;
+      })
+      done();
+  })
+
+  it('should require at least 2 sentences in the paragraph', done => {
+    chai.request(server)
+      .post('/word_count_per_sentence')
+      .send({paragraph: "blah blah blah."})
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.body.should.be.empty;
+      })
+      done();
+  })
+
+  it('should return a response with one key: sentences and one array value with an item for each sentence', done => {
+    chai.request(server)
+      .post('/word_count_per_sentence')
+      .send({paragraph: "blah blah blah. Blah? Testing with a paragraph of various sentence lengths! AND CAPITALS."})
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.an('object');
+        res.body.sentences.should.be.an('array');
+        res.body.sentences.length.should.be.eql(4);
+        res.body.sentences[0].text.should.be.eql('blah blah blah.')
+        res.body.sentences[0].wordCount.should.be.eql(3);
+      })
+      done();
+  })
+
+  it ('should handle white space and no space around sentences', done => {
+    chai.request(server)
+    .post('/word_count_per_sentence')
+    .send({paragraph: "              blah blah blah.     Blah?Testing with a paragraph of various sentence lengths! AND CAPITALS. "})
+    .end((err, res) => {
+      res.should.have.status(200);
+      res.body.should.be.an('object');
+      res.body.sentences.should.be.an('array');
+      res.body.sentences.length.should.be.eql(4);
+      res.body.sentences[0].text.should.be.eql('blah blah blah.')
+      res.body.sentences[0].wordCount.should.be.eql(3);
+    })
+    done(); 
+  }) 
+
+  it ('should handle double punctuation marks', done => {
+    chai.request(server)
+    .post('/word_count_per_sentence')
+    .send({paragraph: "blah blah blah. Blah? Testing with a paragraph of various sentence lengths!? AND CAPITALS."})
+    .end((err, res) => {
+      res.should.have.status(200);
+      res.body.should.be.an('object');
+      res.body.sentences.should.be.an('array');
+      res.body.sentences.length.should.be.eql(4);
+      res.body.sentences[0].text.should.be.eql('blah blah blah.')
+      res.body.sentences[0].wordCount.should.be.eql(3);
+    })
+    done(); 
+  }) 
+
+})
