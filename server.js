03c5cde942ee0a92199378abb4a642d50a5ae9ee
Alicia Hetrick <ahetrick22@gmail.com>
2019-02-07 15:29:16 -0500

    POST word count and paragraph route working

--- server.js
+++ server.js
@@ -12,15 +12,56 @@ app.use(
   })
 );
 
-//The payload should be a simple key/value pair, 
-//where the key is a name or identifier for the paragraph to be evaluated and the value is a string with two or more sentences.
-
-//Return individual sentences along with a word count for the sentence. 
-//A sentence is any grouping of letters and words ending in a period, question mark or exclamation point. 
-//A word is text delimited by spaces.
-//accepts and returns JSON
+//receives a JSON payload in the request body with key 'paragraph' and a string value of 2+ sentences, 
+//returns JSON with key 'sentences' and an array value containing objects with text and wordCount properties
 app.post('/word_count_per_sentence', (req, res) => {
-  res.send('word count route');
+  let { paragraph } = req.body;
+  //make sure the request has sent a paragraph
+  if (paragraph) {
+    //remove whitespace on either side 
+    paragraph = paragraph.trim();
+    //we only want to have to iterate over the entire string once, 
+    //so identify both the spaces and sentence endings in one place and 
+    //then save it in a return obj
+    const returnObj = {
+      sentences: []
+    };
+    //track the previous end so that we know where to split each sentence
+    let previousSentenceEnd = 0;
+    let singleSentence = {
+      text: '', 
+      wordCount: 1
+    };
+    for (let c = 0; c < paragraph.length; c++) {
+      //if the character is a space, then track a new word
+      if (paragraph[c] === ' ') {
+        singleSentence.wordCount++;
+      }
+      //if the character is a sentence ender, then slice off the sentence and push a copy of 
+      //it into the return, then reset the object to track the next sentence
+      if(paragraph[c] === '.' || paragraph[c] === '?' || paragraph[c] === '!') {
+        singleSentence.text = paragraph.slice(previousSentenceEnd, c+1).trim();
+        returnObj.sentences.push({...singleSentence});
+        previousSentenceEnd = c;
+        singleSentence.text = '';
+        singleSentence.wordCount = 1;
+        //bump up to the next index that has a letter after the end of the sentence 
+        //this allows for any number of spaces after a sentence's ending punctuation
+          while (paragraph[c+1] === ' ') {
+            c++
+          };
+      };
+    };
+    //make sure there is more than one sentence in the paragraph, since that was a requirement
+    if (returnObj.sentences.length < 2) {
+      res.status(400).send('Bad request - your paragraph must have 2 or more sentences to be processed');
+    };
+    res.send(JSON.stringify(returnObj));
+  } else {
+    //handle if no paragraph was sent
+    res.status(400).send('Bad request - must send a paragraph in the body');
+  }
+
 })
 
 //For each case-insensitive letter, return the number of times that letter appears in the entire text. 
